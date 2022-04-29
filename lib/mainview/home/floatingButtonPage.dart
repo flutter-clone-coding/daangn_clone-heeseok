c15dac4b9371580da216a20dffcf747499cee579
HeeSeok-Kwon <simondodream@gmail.com>
2022-04-29 11:01:28 +0900

    feat: floatingbutton page, mynear page 기능 추가

--- lib/mainview/home/floatingButtonPage.dart
+++ lib/mainview/home/floatingButtonPage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class FloatingButtonPage extends StatefulWidget {
-  const FloatingButtonPage({Key? key}) : super(key: key);
+  final dx;
+  final dy;
+  const FloatingButtonPage({Key? key, required this.dx, required this.dy}) : super(key: key);
 
   @override
   _FloatingButtonPageState createState() => _FloatingButtonPageState();
@@ -9,10 +11,34 @@ class FloatingButtonPage extends StatefulWidget {
 
 class _FloatingButtonPageState extends State<FloatingButtonPage> {
 
+  final _tapIcon = [
+    Icons.work,
+    Icons.assignment,
+    Icons.set_meal,
+    Icons.apartment,
+    Icons.drive_eta,
+  ];
+
+  final _tapText = [
+    "알바",
+    "과외/클래스",
+    "농수산물",
+    "부동산",
+    "중고차",
+  ];
+
+  final _tapColor = [
+    Colors.deepOrange,
+    Colors.green,
+    Colors.grey,
+    Colors.blueAccent,
+    Colors.blue,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.black38.withOpacity(0.2),
+      color: Colors.black38.withOpacity(0.6),
       child: Stack(
         children: [
           Positioned(
@@ -21,7 +47,90 @@ class _FloatingButtonPageState extends State<FloatingButtonPage> {
             top: 0,
             bottom: 0,
             child: GestureDetector(
-              onTap: () => Navigator.pop(context),
+              onTap: () {
+                print("floating button pop click any position");
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          Positioned(
+            left: widget.dx,
+            top: widget.dy,
+            child: FloatingActionButton(
+              elevation: 0,
+              highlightElevation: 25,
+              backgroundColor: Colors.white,
+              child: const Icon(
+                Icons.close,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                print("pop click floating action x");
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          Positioned(
+            right: widget.dx - 305,
+            bottom: widget.dy - 450,
+            child: Container(
+              width: 200,
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                children: List.generate(5, (index) {
+                  return Container(
+                    margin: const EdgeInsets.only(top: 10, bottom: 10, left: 15),
+                    child: Row(
+                      children: [
+                        Icon(
+                          _tapIcon[index],
+                          color: _tapColor[index],
+                          size: 30,
+                        ),
+                        const SizedBox(width: 20,),
+                        Text(
+                          _tapText[index],
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+          Positioned(
+            right: widget.dx - 305,
+            bottom: widget.dy - 510,
+            child: Container(
+              padding: const EdgeInsets.only(top: 10, bottom: 10, left: 20),
+              width: 200,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.inventory,
+                    color: Colors.brown,
+                  ),
+                  SizedBox(width: 20,),
+                  Text(
+                    "내 물건 팔기",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
--- lib/mainview/home/home.dart
+++ lib/mainview/home/home.dart
@@ -63,131 +63,129 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: List.generate(15, (index) {
-          return Container(
-            decoration: const BoxDecoration(
-              border: Border(bottom: BorderSide(color: Colors.black12)),
-            ),
-            margin: const EdgeInsets.only(left: 10, right: 10),
-            padding: const EdgeInsets.only(top: 10, bottom: 10),
-            child: Row(
-              children: <Widget>[
-                Container(
-                  width: 120,
-                  height: 120,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                    border: Border.all(color: Colors.black12),
-                  ),
-                  child: FittedBox(
-                    child: Image(
-                      image: AssetImage(image[index%10]),
-                    ),
-                    fit: BoxFit.fill,
-                  ),
+    return ListView(
+      children: List.generate(15, (index) {
+        return Container(
+          decoration: const BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.black12)),
+          ),
+          margin: const EdgeInsets.only(left: 10, right: 10),
+          padding: const EdgeInsets.only(top: 10, bottom: 10),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 120,
+                height: 120,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black12),
                 ),
-                Container(
-                  width: MediaQuery.of(context).size.width - 120 - 40,
-                  height: 120,
-                  margin: const EdgeInsets.only(left: 10),
-                  child: Column(
-                    children: [
-                      Container(
-                        alignment: Alignment.topLeft,
-                        margin: const EdgeInsets.only(bottom: 3),
-                        child: Text(
-                          title[index%10],
-                          style: const TextStyle(
-                            fontSize: 18,
-                            color: Colors.black,
-                          ),
+                child: FittedBox(
+                  child: Image(
+                    image: AssetImage(image[index%10]),
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width - 120 - 40,
+                height: 120,
+                margin: const EdgeInsets.only(left: 10),
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.topLeft,
+                      margin: const EdgeInsets.only(bottom: 3),
+                      child: Text(
+                        title[index%10],
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
                         ),
                       ),
-                      Container(
-                        alignment: Alignment.topLeft,
-                        margin: const EdgeInsets.only(bottom: 3),
-                        child: Text(
-                          "${region[index%10]} · 끌올 ${index+1}분전",
-                          style: const TextStyle(
-                            fontSize: 13,
-                            color: Colors.black38,
-                          ),
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      margin: const EdgeInsets.only(bottom: 3),
+                      child: Text(
+                        "${region[index%10]} · 끌올 ${index+1}분전",
+                        style: const TextStyle(
+                          fontSize: 13,
+                          color: Colors.black38,
                         ),
                       ),
-                      Container(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          price[index%10],
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        price[index%10],
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      const Spacer(),
-                      Row(
-                        children: [
-                          const Spacer(),
-                          Container(
-                            margin: const EdgeInsets.only(right: 3),
-                            child: const Icon(
-                              Icons.chat_bubble_outline,
-                              size: 18,
+                    ),
+                    const Spacer(),
+                    Row(
+                      children: [
+                        const Spacer(),
+                        Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const Icon(
+                            Icons.chat_bubble_outline,
+                            size: 18,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 5),
+                          child: const Text(
+                            '2',
+                            style: TextStyle(
+                                fontSize: 18
                             ),
                           ),
-                          Container(
-                            margin: const EdgeInsets.only(right: 5),
-                            child: const Text(
-                              '2',
-                              style: TextStyle(
-                                  fontSize: 18
-                              ),
-                            ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const Icon(
+                            Icons.question_answer_outlined,
+                            size: 18,
                           ),
-                          Container(
-                            margin: const EdgeInsets.only(right: 3),
-                            child: const Icon(
-                              Icons.question_answer_outlined,
-                              size: 18,
-                            ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(right: 3),
-                            child: const Text(
-                              '3',
-                              style: TextStyle(
-                                  fontSize: 18
-                              ),
-                            ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(right: 3),
-                            child: const Icon(
-                              Icons.favorite_border,
-                              size: 18,
-                            ),
-                          ),
-                          const Text(
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const Text(
                             '3',
                             style: TextStyle(
-                              fontSize: 18,
+                                fontSize: 18
                             ),
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 3),
+                          child: const Icon(
+                            Icons.favorite_border,
+                            size: 18,
+                          ),
+                        ),
+                        const Text(
+                          '3',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          );
-        }),
-      ),
+              ),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
